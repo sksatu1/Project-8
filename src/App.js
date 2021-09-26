5ea2810f069a2ed9abf5a429c1af79d9ca894d46
sksatu1 <satueee18@gmail.com>
2021-09-26 21:51:46 +0600

    15th

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Writers from './components/Writers/Writers';
 
 function App() {
-  /*  useEffect(() => {
-     fetch("./writers.json")
-       .then(res => res.json())
-       .then(data => console.log(data))
-   }, []) */
   return (
     <div className="App">
       <Header></Header>
